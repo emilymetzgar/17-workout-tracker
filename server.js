e9b3dd66c66627102a1d62b928be33c64d26f027
emilymetzgar93 <emilymetzgar93@gmail.com>
2021-06-07 11:55:14 -0500

    deployling"

--- server.js
+++ server.js
@@ -12,10 +12,11 @@ app.use(express.urlencoded({
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
+    useFindAndModify: false
 });
 
 app.use(require('./controllers/api'));
